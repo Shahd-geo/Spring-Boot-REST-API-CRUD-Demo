d199815c7b018535b17a68429964c1dc69439510
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 07:42:45 +0400

    create alist of student

--- SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
+++ SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 
 public class StudentContlor {
@@ -16,6 +18,11 @@ public class StudentContlor {
     @PostMapping("/addStudent")
     public Student postDetails(@RequestBody Student student) {
         return studentService.addStudent(student);
+
+    }
+    @PostMapping("/addStudentList")
+    public List<Student> postDetails(@RequestBody List<Student> students) {
+        return studentService.addListStudent(students);
 
     }
 
--- SpringPro/src/main/java/com/example/SpringPro/Service/StudentService.java
+++ SpringPro/src/main/java/com/example/SpringPro/Service/StudentService.java
@@ -3,10 +3,16 @@ package com.example.SpringPro.Service;
 import com.example.SpringPro.Entity.Student;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
     public Student addStudent(Student student){
         return student;
     }
+
+        public List<Student> addListStudent(List<Student> students){
+            return students;
+        }
 }
